5b26ee23d009c70036233c5417a704ef2d4e1da6
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-02-23 09:53:24 -0700

    added list methods

--- MegaProject/Model/List.hpp
+++ MegaProject/Model/List.hpp
@@ -21,12 +21,14 @@ class List
 private:
     int size;
     Node<Type>* front;
+    Node<Type> end;
 public:
     List<Type>();
     
     ~List<Type>();
     List<Type>(const Array<Type> & toBeCopied);
     //void operator = (const Array<Type> & toBeAssigned);
+    
     
     void addAtIndex(int index, Type value);
     void add(Type value);
@@ -36,8 +38,191 @@ public:
     bool contains(Type data);
     int getSize() const;
     Node<Type>* getFront() const;
+    Node<Type>* getEnd() const;
     
     
 };
+
+template <class Type>
+List<Type> :: List()
+{
+    this->size = 0;
+    this->front = nullptr;
+    this->end = nullptr;
+    
+}
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    Node<Type> * first = new Node(value);
+
+    
+    if(size == 0)
+    {
+        //first node in the list, (Front)
+        this->front = first;
+        this->end = first;
+        
+    }
+    else
+    {
+       // Node<Type> * newFirst = new Node<type>(value);
+        first->setNodePointer(front);
+        
+        front = first;
+    }
+    size++;
+   
+}
+
+template <class Type>
+void List<type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size==0)
+    {
+        this->front = added;
+        this->end = added;
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+}
+
+template <class Type>
+void List<Type> :: adAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index <= size);
+    if(index==0)
+    {
+        addFront(value);
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+    }
+    else
+    {
+       
+        Node<Type> insertedNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * previous = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current-> getNodePointer();
+            
+        }
+        
+        previous->setNodePointer(insertedNode);
+        insertedNode->setNodePointer(current);
+        
+        
+        size++;
+
+    }
+    
+    
+    
+    
+    
+    
+}
+
+template <class Type>
+Type List<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < size);
+    Type value;
+    
+    Node<Type> * current = front;
+    
+    for(int position = 0; position < index; position++)
+    {
+        current = current->getNodePointer();
+    }
+    value = current->getNodeData();
+    return value;
+    
+    
+}
+
+template <class Type>
+Type Array<Type> :: setAtIndex(int index, Type value)
+{
+    assert(index>= 0 && index < size);
+    Node<Type> * current = front;
+    for(int position = 0; position < index; position ++)
+    {
+        current = current->getNodePointer();
+        
+    }
+    
+    current->setNodeData(value);
+    return current;
+}
+
+template <class Type>
+Type List<Type> :: remove(int index)
+{
+    
+    assert(index>= 0 && index < size);
+    
+    Type removed;
+    
+    Node<Type> * current = front;
+    Node<Type> * previous = nullptr;
+    Node<Type> * toBeRemoved = nullptr;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        this->front = front->getNodePointer();
+    }
+    else if(index == size-1)
+    {
+        for(int spot = 0; spot < index: spot++)
+        {
+            previouse = current;
+            current = current->getNodePointer();
+            
+        }
+
+        toBeRemoved = current;
+        previous->setNodePointer(nullptr);
+        this->end = previous;
+    }
+    else
+    {
+        for(int spot = 0; spot < index: spot++)
+        {
+            previouse = current;
+            current = current->getNodePointer();
+            
+        }
+        
+        toBeRemoved = current;
+        current = toBeRemoved->getNodePointer();
+        previous->setNodePointer(current);
+    }
+    
+    
+    
+   
+    
+    removed = toBeRemoved->getNodeData();
+    
+    delete toBeRemoved;
+    
+    size--;
+    return removed;
+}
+
+
+
 
 #endif /* List_h */
