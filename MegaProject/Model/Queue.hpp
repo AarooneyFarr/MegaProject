6f0c5247a06604e4f0a2528ec6e11a7aa90332d5
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-03-07 08:43:14 -0700

    added to queue

--- MegaProject/Model/Queue.hpp
+++ MegaProject/Model/Queue.hpp
@@ -15,9 +15,79 @@ template<class Type>
 class Queue : public DoublyLinkedList<Type>
 {
 private:
+    BiDirectionalNode<Type> * front;
+    BiDirectionalNode<Type> * end;
+    int size;
 public:
+    
+    Queue();
+    ~Queue();
+    
+    void add(Type data);
+    Type remove(int index);
+    
+    void enqueue(Type data);
+    Type dequeue();
+    Type peek();
+}
+
+
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index == 0 && this->getSize() > 0);
+    return dequeue;
     
 }
 
 
+
+/*
+ 1. Assert size is valid.
+ 2. Get data from front.
+ 3. If size == 1 set end to nullptr.
+ 4. Else, move front to fronts next.
+ 4a. Set fronts previous to nullptr.
+ 5. delete old front node.
+ 6. adjust size down by 1.
+ 7. return old value.
+ */
+template <class Type>
+Type Queue<Type> :: Dequeue()
+{
+    assert(this->getSize() == 1)
+    
+    Type removedValue = this->getFront()->getNodeData();
+    BiDirectionalNode<Type> * removeMe = this->getFront();
+    
+    if(this->getSize() == 1)
+    {
+        this->setFront(nullptr);
+        this->setEnd(nullptr);
+        
+    }
+    else
+    {
+        this->setFront(removeMe->getNextPointer);
+    }
+    this->setFront()->setPreviousPointer(nullptr);
+    
+    delete removeMe;
+    this->setSize(this->getSize()-1);
+    
+    return removedValue;
+}
+
+/*
+ 1. Check that the size is greater than 0.
+ 2. Return the front objects data.
+ */
+
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+    
+    return this->getFront()->getNodeData();
+}
 #endif /* Queue_h */
