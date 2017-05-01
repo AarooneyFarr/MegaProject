ecdedcc8d8ffd611c3069b55a1861ad781630d00
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-05-01 09:54:44 -0600

    added hash

---
+++ MegaProject/Model/HashTable.hpp
@@ -0,0 +1,123 @@
+//
+//  HashTable.hpp
+//  MegaProject
+//
+//  Created by Farr, Aaron on 5/1/17.
+//  Copyright © 2017 Farr, Aaron. All rights reserved.
+//
+
+#ifndef HashTable_h
+#define HashTable_h
+
+#include <cmath>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class HashTable
+{
+private:
+    long capacity;
+    long sze;
+    double efficiencyPercentage;
+    Type * * hashTableStorage;
+    bool isPrime(long sampleNumber);
+    void resize();
+    long nextPrime(long current);
+    long findPosition(Type data);
+    long handleCollision(Type data, long currentPosition);
+public:
+    HashTable();
+    ~HashTable();
+    void add(Type data);
+    bool remove(Type data);
+    void displayContents();
+    
+    
+};
+
+template <class Type>
+HashTable<Type> :: HashTable()
+{
+    this->capacity = 101;
+    this->efficiencyPercentage = .667;
+    this->size = 0;
+    this->hashTableStorage = Type * [capacity];
+}
+
+template <class Type>
+HashTable<Type> :: ~HashTable()
+{
+    delete [] hashTableStorage;
+}
+
+template <class Type>
+bool HashTable<Type> :: isPrime(long candidateNumber)
+{
+    if(candidate <= 1)
+    {
+        return false;
+    }
+    else if(candidateNumber == 2 || candidateNumber == 3)
+    {
+        return true;
+    }
+    else if(candidateNumber % 2 == 0)
+    {
+        return false;
+        
+    }
+    else
+    {
+        for(int next = 3; next <= sqrt(candidateNumber) +1; next += 2)
+        {
+            if(candidateNumber % next == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
+
+template <class Type>
+int HashTable<Type> :: getNextPrime()
+{
+    int nextPrime = (this->capacity * 2) + 1;
+    
+    while(!isPrime(nextPrime))
+    {
+        nextPrime++;
+    }
+    
+    return nextPrime;
+}
+
+template <class Type>
+long HashTable<Type> :: findPosition(HashNode<Type> * data)
+{
+    long insertPosition = data->getKey() % this->capacity;
+    return insertPosition;
+}
+
+template <class Type>
+long HashTable<Type> :: handleCollision(HashNode<Type> * data, long currentPosition)
+{
+    long shift = 17;
+    
+    for(long position = currentPosition + shift; position != currentPosition; position += shift)
+    {
+        if(position > capacity)
+        {
+            position = position % capacity;
+        }
+        
+        if(hashTableStorage[position] == nullptr)
+        {
+            return position;
+        }
+    }
+}
+
+#endif /* HashTable_h */
